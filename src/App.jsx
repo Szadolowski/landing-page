7e27c069bf035dc489da4a56f37715dd9079a0d5
Szadolowski <rafall.curzydlo@gmail.com>
2024-06-30 23:13:58 +0200

    0.1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <header className="flex flex-row justify-between bg-black text-white py-8 px-10 mx-60 box-border">
+        <img src="" alt="img" />
+        <div className="flex justify-between align-middle p-2.5 box-border space-x-7">
+          <p>Home</p>
+          <p>Product</p>
+          <p>About</p>
+          <p>Contact</p>
+        </div>
+        <button>Login</button>
+      </header>
+    </>
+  );
+}
+
+export default App;
